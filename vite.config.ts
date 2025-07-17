b6310453432ec1c5064f660b1819a61db646da46
shhaakil <159673524+shhaakil@users.noreply.github.com>
2025-07-17 10:40:35 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    // 👇 ADD THIS LINE
-    base: '/image-compressor/',
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
 
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    }
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: isGithubPages ? '/image-compressor/' : '/',
+})
